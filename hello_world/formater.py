06a019ecc87feb84b71bf97b55dc77201b8aa949
marszalekanna <marszalek.annalaura@gmail.com>
2022-05-14 12:19:34 +0200

    Początek XML

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,10 +1,13 @@
+# from hello_world.views import moje_imie
+
 
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
 
 def get_formatted(msg, imie, format):
@@ -17,12 +20,17 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
+    return ('{ "imie":"' + imie + '", "mgs":"' +
             msg + '"}')
+
+def format_to_xml(msg, imie):
+    return ("greetings" + "<" + "<a>" + imie + "</a>" + ">")
 
 
 def plain_text(msg, imie):
