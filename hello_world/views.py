34cbe414c5137ce7f9f720e2df9e98f3f6468132
Kaajak <95999361+Kaajak@users.noreply.github.com>
2022-05-14 11:02:19 +0000

    Zmiana imienia na drugie

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Ania"
+moje_imie = "Laura"
 msg = "Hello World!"
 
 @app.route('/') # noqa
